bd0f48f192eb9aed97dd8ee5f6ca638bf52e0ca2
sania <sania.nisar01@gmail.com>
2025-03-15 02:03:53 +0500

    Main Activity/Home Screen Implemented

--- app/src/main/java/com/app/mycvbuilderapp/MainActivity.java
+++ app/src/main/java/com/app/mycvbuilderapp/MainActivity.java
@@ -1,24 +1,28 @@
 package com.app.mycvbuilderapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Set up button click listeners
+        findViewById(R.id.btnProfilePic).setOnClickListener(view -> openActivity(ProfileActivity.class));
+        findViewById(R.id.btnPersonalDetails).setOnClickListener(v -> openActivity(PersonalDetailsActivity.class));
+        findViewById(R.id.btnSummary).setOnClickListener(v -> openActivity(SummaryActivity.class));
+        findViewById(R.id.btnEducation).setOnClickListener(v -> openActivity(EducationActivity.class));
+        findViewById(R.id.btnExperience).setOnClickListener(v -> openActivity(ExperienceActivity.class));
+        findViewById(R.id.btnReferences).setOnClickListener(v -> openActivity(ReferencesActivity.class));
+    }
+
+    private void openActivity(Class<?> activityClass) {
+        Intent intent = new Intent(MainActivity.this, activityClass);
+        startActivity(intent);
     }
 }
